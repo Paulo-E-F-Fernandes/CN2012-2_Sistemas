dab5319fe9ce5b022a9245fc16374fe6ed5f4941
Paulo Fernandes <paulo@paulo-laptop.(none)>
2013-01-19 01:47:36 -0200

    Criado o projeto no eclipse, as classe Loading(Runnable) e ResolucaoSistema(main) e o diretório Documentos. Adicionei também a imagem Anexo001.PNG

---
+++ src/Loading.java
@@ -0,0 +1,14 @@
+
+public class Loading implements Runnable {
+	
+	//Interface interfaceGrafica = new Interface();
+	
+	public void run() {
+		// interfaceGrafica.loadingStart();
+	}
+	
+	public void exit() {
+		// interfaceGrafica.loadingStop();
+	}
+
+}
---
+++ src/ResolucaoSistemas.java
@@ -0,0 +1,8 @@
+
+public class ResolucaoSistemas {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
