b44fd32651db5686418c688200073d270b07b9f6
Paulo Fernandes <paulo@paulo-laptop.(none)>
2013-02-07 23:43:17 -0200

    Gauss-Seidel pronto!

--- src/GaussSeidel.java
+++ src/GaussSeidel.java
@@ -1,69 +1,103 @@
+
 public class GaussSeidel extends GaussJacobi {
-	//private double []resultado;
-	//private int iteracoes = 0;
-	//private double erro;
 	
-	/*
-	public double[] getResultado() {
+	/* metodo que executa o metodo de Gauss-Jacobi do calculo numerico
+     * 
+     * @param Matriz, double[], double
+     * @return double[]
+     */
+    public double[] executar (Matriz m, double[] vetor, double erro) {
+		this.matriz = new Matriz(m.linhas, m.colunas);
+		this.matriz.setMatrizAmpliada(m.matrizAmpliada);
+		this.resultado = new double[m.linhas];
+		
+		if (!this.verificarConvergencia()) {
+			for(int i = 0; i < this.matriz.linhas; i++) {
+				this.resultado[i] = 0;
+				this.erro = 0;
+			}
+        }
+        else {
+        	this.erro = erro;
+    		this.calcular(vetor);
+    		this.printaResultados();
+        }
 		return this.resultado;
 	}
-	
-	public void printaResultados() {
-		System.out.println("--Gauss Seidel--");
-		for(int i = 0; i < this.resultados.length; i++) {
-			System.out.println("x"+i+" = "+this.resultado[i]);
-		}
-		System.out.println("Número de iterações = "+this.iteracoes);
-		System.out.println("Erro = "+this.erro);
-	}
-	*/
-	public double[] executar(Matriz m, double[] vetor, double erro) {
-		double []anterior = new double[m.linhas];
-		double []atual = new double[m.linhas];
-		//this.resultado = new double[m.linhas];
-		//this.erro = erro;
-		
-		//Syste,.arraycopy(vetor, 0, this.resultado, 0, vetor.length);
-		for(int i = 0; i < m.linhas; i++) {
-			atual[i] = vetor[i];
-		}
-		
-		do {
-			for(int i = 0; i < m.linhas; i++) {
-				//System.arraycopy(this.resultado, 0, anterior, 0, resultado.length);
-				System.arraycopy(atual, 0, anterior, 0, atual.length);
-				//this.resultado[i] = m.termosIndependentes[i] / m.matriz[i][i];
-				atual[i] = m.termosIndependentes[i] / m.matriz[i][i];
-				for(int j = 0; j < m.linhas; j++) {
+    
+    /* metodo que calcula o resultado
+     * 
+     * @param double[]
+     * @return void
+     */
+    protected void calcular(double[] vetor) {
+    	double []anterior = new double[this.matriz.linhas];
+    	
+    	System.arraycopy(vetor, 0, this.resultado, 0, vetor.length);
+    	
+    	do {
+			for(int i = 0; i < this.matriz.linhas; i++) {
+				System.arraycopy(this.resultado, 0, anterior, 0, resultado.length);
+				this.resultado[i] = this.matriz.termosIndependentes[i] / this.matriz.matriz[i][i];
+				for(int j = 0; j < this.matriz.linhas; j++) {
 					if(i != j) {
-						//this.resultado[i] = this.resultado[i] - ((m.matriz[i][j] * this.resultado[j]) / m.matriz[i][i]);
-						atual[i] = atual[i] - ((m.matriz[i][j] * atual[j]) / m.matriz[i][i]);
+						this.resultado[i] = this.resultado[i] - ((this.matriz.matriz[i][j] * this.resultado[j]) / this.matriz.matriz[i][i]);
 					}
 				}
 			}
-			//this.iteracoes = this.iteracoes + 1;
-		//} while(!testar(this.resultado, anterior, erro));	
-		} while(!testar(atual, anterior, erro));
-		//this.printaResultados();
-		return atual;
-	}
-/*	
-	public boolean sassenfeld() {
-		//mc
+			this.iteracoes = this.iteracoes + 1;	
+		} while(!testeParada(this.resultado, anterior));
+    }
+	
+	/* metodo que verifica a convergencia
+     * 
+     * @param void
+     * @return boolean
+     */
+    protected boolean verificarConvergencia() {
+    	boolean converge = false;
+        for (int i = 0; i < this.matriz.linhas && !converge; i++) {
+        	for (int j = i + 1; j < this.matriz.linhas && !converge; j++) {
+        		
+        		if (this.criterioLinhas() && this.sassenfeld()) {
+        			converge = true;
+        		}
+        		else if (this.sassenfeld()) {
+        			converge = true;
+        		}
+        		else {
+        			converge = false;
+        		}
+        		
+        		if(!converge) {
+        			trocarLinhas(i,j);
+        		}
+        	}
+        }
+    	return converge;    
+    }
+
+	/* metodo que calcula o criterio de sassenfeld
+     * 
+     * @param void
+     * @return boolean
+     */
+    private boolean sassenfeld() {
 		double beta = 0;
-		double []betaParcial = new double[mc.linha];
+		double []betaParcial = new double[this.matriz.linhas];
+		int i;
 		
-		for(int i = 0; i < mc.linhas; i++) {
+		for(i = 0; i < this.matriz.linhas; i++) {
 			betaParcial[i] = 1;
 		}
 		
-		for(int i = 0; i < mc.linhas; i++) {
-			for(int j = 0; j < mc.linhas; j++) {
+		for(i = 0; i < this.matriz.linhas; i++) {
+			for(int j = 0; j < this.matriz.linhas; j++) {
 				if(i != j) {
-					beta = beta + (Math.abs(mc[i][j]) * betaParcial[j]);
+					beta = beta + (Math.abs(this.matriz.matriz[i][j]) * betaParcial[j]);
 				}
 			}
-			betaParcial[i] = beta / Math.abs(mc[i[i);
+			betaParcial[i] = beta / Math.abs(this.matriz.matriz[i][i]);
 			if(betaParcial[i] > 1) {
 				return false; // Não converge
 			}
@@ -74,5 +108,4 @@ public class GaussSeidel extends GaussJacobi {
 		
 	}
 	
-*/
 }
